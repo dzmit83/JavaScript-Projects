c936a25a7ce949e846f0e01a8e5c292e063481a2
dzmit83 <dzmitry.fedotov@inbox.lv>
2023-12-02 16:32:52 +0000

    Project2_functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {//+=operator function
+    var sentence = "Summer is the most";//creating variable
+    sentence += " beautiful season of the year.";//adding to the variable
+    document.getElementById("Concatenate").innerHTML = sentence;//using document.getElementById
+ }
